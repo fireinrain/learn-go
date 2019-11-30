70bed64c6f9162ea101d0c2e3656708fb60c0353
lzhaoyang <575563079@qq.com>
2019-11-30 22:07:02 +0800

    update

--- src/chapter7/Cli.go
+++ src/chapter7/Cli.go
@@ -13,19 +13,19 @@ func main() {
 
 	closer, e := command.StdoutPipe()
 	if e != nil {
-		log.Fatal(e)
+		log.Println(e)
 	}
 	//及时关闭
 	defer closer.Close()
 	//运行
-	start := command.Run()
+	start := command.Start()
 	if start != nil {
-		log.Fatal(start)
+		log.Println(start)
 	}
 	//读取输出结果
 	readAll, e := ioutil.ReadAll(closer)
 	if e != nil {
-		log.Fatal(e)
+		log.Println(e)
 	}
 	fmt.Println("读取到命令输出： ", string(readAll))
 
